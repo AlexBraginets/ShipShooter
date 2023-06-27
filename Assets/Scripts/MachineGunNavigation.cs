815301db5c9c0aacee3125e5d6c4574777e9be94
AlexBraginets <qetting@gmail.com>
2023-06-27 18:22:00 +0300

    IronSightHandler

--- Assets/Scripts/MachineGunNavigation.cs
+++ Assets/Scripts/MachineGunNavigation.cs
@@ -7,8 +7,10 @@ public class MachineGunNavigation : MonoBehaviour
     [SerializeField] private float _editorSpeed;
     [SerializeField] private Vector2 _yawLimit;
     [SerializeField] private Vector2 _pitchLimit;
+    [SerializeField] private IronSightHandler _ironSightHandler;
     private float _yaw;
     private float _pitch;
+    private const float MOVING_THRESHOLD = .0001f;
 
     private float _lookSpeed
     {
@@ -28,10 +30,20 @@ public class MachineGunNavigation : MonoBehaviour
 
     private void Update()
     {
-        _yaw += _lookSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
-        _pitch -= _lookSpeed * Time.deltaTime * Input.GetAxis("Mouse Y");
+        float dYaw = _lookSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+        float dPitch = _lookSpeed * Time.deltaTime * Input.GetAxis("Mouse Y");
+        _yaw += dYaw;
+        _pitch -= dPitch;
         _yaw = Mathf.Clamp(_yaw, _yawLimit.x, _yawLimit.y);
         _pitch = Mathf.Clamp(_pitch, _pitchLimit.x, _pitchLimit.y);
         transform.localRotation = Quaternion.AngleAxis(_yaw, Vector3.up) * Quaternion.AngleAxis(_pitch, Vector3.right);
+        if (new Vector2(dYaw, dPitch).magnitude > MOVING_THRESHOLD)
+        {
+            _ironSightHandler.IsMoving = true;
+        }
+        else
+        {
+            _ironSightHandler.IsMoving = false;
+        }
     }
 }
--- Assets/Scripts/UI/IronSight.cs
+++ Assets/Scripts/UI/IronSight.cs
@@ -1,32 +1,12 @@
-using DG.Tweening;
 using UnityEngine;
 
 namespace UI
 {
     public class IronSight : MonoBehaviour
     {
-        [SerializeField] private float _animationDuration;
         [SerializeField] private CrossView[] _crossViews;
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                Animate();
-            }
-        }
-
-        private void Animate()
-        {
-            var sequence = DOTween.Sequence();
-            var tween = DOTween.To(() => 0, SetNormalizedOffset, 1f, _animationDuration / 2f);
-            sequence.Append(tween);
-            tween = DOTween.To(() => 1f, SetNormalizedOffset, 0f, _animationDuration / 2f);
-            sequence.Append(tween);
-            sequence.Play();
-        }
-
-        private void SetNormalizedOffset(float normalizedOffset)
+        public void SetNormalizedOffset(float normalizedOffset)
         {
             foreach (var crossView in _crossViews)
             {
