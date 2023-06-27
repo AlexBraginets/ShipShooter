8e3317518d36c0f8bac4c67e345fbf4624e682a3
AlexBraginets <qetting@gmail.com>
2023-06-27 18:37:11 +0300

    Bullet prefab

--- Assets/Scripts/MachineGun.cs
+++ Assets/Scripts/MachineGun.cs
@@ -3,39 +3,52 @@ using UnityEngine;
 
 public class MachineGun : MonoBehaviour
 {
-   [SerializeField] private GameInput _gameInput;
-   [SerializeField] private ShootActionWrapper _shootActionWrapper;
-   [SerializeField] private LayerMask _targetLayerMask;
-   [SerializeField] private Camera _camera;
-   public event Action OnStartShooting;
-   public event Action OnStopShooting;
-   private void Start()
-   {
-      _gameInput.OnStartShooting += StartShooting;
-      _gameInput.OnStopShooting += StopShooting;
-      _shootActionWrapper.OnShoot += Shoot;
-   }
+    [SerializeField] private GameInput _gameInput;
+    [SerializeField] private ShootActionWrapper _shootActionWrapper;
+    [SerializeField] private LayerMask _targetLayerMask;
+    [SerializeField] private Camera _camera;
+    public event Action OnStartShooting;
+    public event Action OnStopShooting;
+    [SerializeField] private Transform _bulletPrefab;
+    [SerializeField] private Transform _shootingPoint;
+    [SerializeField] private float _bulletSpeed;
 
-   private void StartShooting()
-   {
-      OnStartShooting?.Invoke();
-   }
+    private void Start()
+    {
+        _gameInput.OnStartShooting += StartShooting;
+        _gameInput.OnStopShooting += StopShooting;
+        _shootActionWrapper.OnShoot += Shoot;
+    }
 
-   private void StopShooting()
-   {
-      OnStopShooting?.Invoke();
-   }
+    private void StartShooting()
+    {
+        OnStartShooting?.Invoke();
+    }
 
-   private void Shoot()
-   {
-      Ray ray = _camera.ViewportPointToRay(Vector3.one * .5f);
-      var raycastDistance = 1000f;
-      if (Physics.Raycast(ray, out RaycastHit hit, raycastDistance, _targetLayerMask))
-      {
-         if (hit.transform.TryGetComponent(out Target target))
-         {
-            target.Destroy();
-         }
-      }
-   }
+    private void StopShooting()
+    {
+        OnStopShooting?.Invoke();
+    }
+
+    private void Shoot()
+    {
+        var bullet = Instantiate(_bulletPrefab);
+        bullet.position = _shootingPoint.position;
+        bullet.rotation = _shootingPoint.rotation;
+        
+        Target target = null;
+        Ray ray = _camera.ViewportPointToRay(Vector3.one * .5f);
+        var raycastDistance = 1000f;
+        if (Physics.Raycast(ray, out RaycastHit hit, raycastDistance, _targetLayerMask))
+        {
+            hit.transform.TryGetComponent(out target);
+        }
+
+        Vector3 shootDirection = _camera.transform.forward;
+        if (target)
+        {
+            shootDirection = (hit.point - _shootingPoint.position).normalized;
+        }
+        bullet.GetComponent<Rigidbody>().velocity = shootDirection * _bulletSpeed;
+    }
 }
