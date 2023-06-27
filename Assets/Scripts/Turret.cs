11a0bdddc2909fe4dfd9d14da2c3456eaeece026
AlexBraginets <qetting@gmail.com>
2023-06-27 23:44:23 +0300

    Turret follow target

--- Assets/Scripts/Turret.cs
+++ Assets/Scripts/Turret.cs
@@ -13,14 +13,14 @@ public class Turret : MonoBehaviour
     private void Update()
     {
         Vector3 direction2Target = (_target.position - transform.position).normalized;
-        direction2Target = transform.InverseTransformDirection(direction2Target);
         var rotation2Target = Quaternion.LookRotation(direction2Target, Vector3.up);
-        var currentRotation = transform.localRotation;
-        var lerpRotation = Quaternion.Lerp(currentRotation, rotation2Target, _rotationSpeed * Time.deltaTime);
+        var currentRotation = transform.rotation;
+        var lerpRotation = Quaternion.Slerp(currentRotation, rotation2Target, _rotationSpeed * Time.deltaTime);
+        lerpRotation = Quaternion.Inverse(transform.parent.rotation) * lerpRotation;
         _yaw = lerpRotation.eulerAngles.y;
-        _pitch = lerpRotation.eulerAngles.z;
+        _pitch = lerpRotation.eulerAngles.x;
         _yaw = Mathf.Clamp(_yaw, _yawLimit.x, _yawLimit.y);
         _pitch = Mathf.Clamp(_pitch, _pitchLimit.x, _pitchLimit.y);
-        transform.rotation = Quaternion.AngleAxis(_yaw, Vector3.up) * Quaternion.AngleAxis(_pitch, Vector3.right);
+        transform.localRotation = Quaternion.AngleAxis(_yaw, Vector3.up) * Quaternion.AngleAxis(_pitch, Vector3.right);
     }
 }
