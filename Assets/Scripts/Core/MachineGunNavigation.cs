cc94b35d898050cf09c84d257a91d5b672243d23
AlexBraginets <qetting@gmail.com>
2023-06-28 17:49:43 +0300

    MachineGunNavigation Input

---
+++ Assets/Scripts/Core/MachineGunNavigation.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UserInput;
+
+namespace Core
+{
+    public class MachineGunNavigation : MonoBehaviour
+    {
+        [SerializeField] private MachineGunNavigationInput _input;
+        [SerializeField] private Vector2 _yawLimit;
+        [SerializeField] private Vector2 _pitchLimit;
+        [SerializeField] private IronSightHandler _ironSightHandler;
+        private float _yaw;
+        private float _pitch;
+        private void Awake()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        private void Update()
+        {
+            _yaw += _input.dYaw;
+            _pitch -= _input.dPitch;
+            _yaw = Mathf.Clamp(_yaw, _yawLimit.x, _yawLimit.y);
+            _pitch = Mathf.Clamp(_pitch, _pitchLimit.x, _pitchLimit.y);
+            transform.localRotation = Quaternion.AngleAxis(_yaw, Vector3.up) * Quaternion.AngleAxis(_pitch, Vector3.right);
+            _ironSightHandler.IsMoving = _input.IsMoving;
+        }
+    }
+}
---
+++ Assets/Scripts/UserInput/MachineGunNavigationInput.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace UserInput
+{
+    [DefaultExecutionOrder(-1)]
+    public class MachineGunNavigationInput : MonoBehaviour
+    {
+        [SerializeField] private float _buildLookSpeed;
+        [SerializeField] private float _editorLookSpeed;
+        public float dYaw => LookSpeed * Time.deltaTime * Input.GetAxis("Mouse X");
+        public float dPitch => LookSpeed * Time.deltaTime * Input.GetAxis("Mouse Y");
+        public bool IsMoving => new Vector2(dYaw, dPitch).magnitude > MOVING_THRESHOLD;
+        private const float MOVING_THRESHOLD = .0001f;
+
+        private float LookSpeed
+        {
+            get
+            {
+#if UNITY_EDITOR
+                return _editorLookSpeed;
+#endif
+                return _buildLookSpeed;
+            }
+        }
+    }
+}
