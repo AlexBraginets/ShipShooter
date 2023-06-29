37a8382d283ae5af4cb9fd5b71dec13f4f3ba5cf
AlexBraginets <qetting@gmail.com>
2023-06-29 16:57:20 +0300

    LosePopup wip

--- Assets/Scripts/UI/Popups/LosePopup.cs
+++ Assets/Scripts/UI/Popups/LosePopup.cs
@@ -1,12 +1,25 @@
+using DG.Tweening;
 using UnityEngine;
 
 namespace UI.Popups
 {
     public class LosePopup : MonoBehaviour
     {
+        [SerializeField] private Animator _animator;
+        [SerializeField] private GameObject _label;
+        [SerializeField] private float _showLabelDelay;
+        private const string SHOW = "Show";
+
         public void Show()
         {
             gameObject.SetActive(true);
+            DOVirtual.DelayedCall(_showLabelDelay, ShowLabel);
+        }
+
+        private void ShowLabel()
+        {
+            _label.SetActive(true);
+            _animator.SetTrigger(SHOW);
         }
     }
 }
